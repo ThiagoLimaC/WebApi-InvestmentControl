2c479da8eff2d1804ef27ab25295288cfcc42c42
Thiago Lima <150944198+ThiagoLimaC@users.noreply.github.com>
2025-06-14 17:58:20 -0300

    Integrar o Entity Framework Core com SQL Server
    Adicionada a integração do EF Core no arquivo Program.cs e configurado o AppDbContext para o SQL Server
    Atualizado o arquivo WebApi-InvestmentControl.csproj com os pacotes NuGet necessários
    Adicionada a string de conexão no appsettings.json. Criados AppDbContext e InvestimentoModel para definir o contexto do banco de dados e a estrutura da entidade.

---
+++ Models/InvestimentoModel.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApi_InvestmentControl.Models
+{
+    public class InvestimentoModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Tipo { get; set; }
+        public decimal Valor { get; set; }
+        public DateTime DataInvestimento { get; set; }
+    }
+}
